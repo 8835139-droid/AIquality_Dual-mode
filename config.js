73ee3e7269a8cd82c1c879bd6dca829f97a27e08
JayWang <8835139@gmail.com>
2025-10-23 17:57:15 +0800

    Update LOCAL_MODEL configuration in config.js
    設定ollama 本地使用模型名稱

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 
-//指定ollama 本地模型llama3.2-vision:11b
+//設定ollama 本地使用模型名稱
 const LOCAL_MODEL = 'qwen2.5vl:7b';
 //const LOCAL_MODEL = 'willqiu/Llama-Breeze2-8B-Instruct';
 module.exports = { LOCAL_MODEL };
+
