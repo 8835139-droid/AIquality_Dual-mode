4442a011a40a1fa520b436686fe08fca35247c41
JayWang <8835139@gmail.com>
2025-10-23 18:02:00 +0800

    可設定更改 ollama 本地使用模型
    可依照個人設備規格使用更大參數規格的模型，例如qwen2.5vl:32b

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 
-//設定ollama 本地使用模型名稱
+//設定ollama 本地使用模型名稱,可依照個人設備規格使用更大參數規格的模型，例如qwen2.5vl:32b
 const LOCAL_MODEL = 'qwen2.5vl:7b';
 //const LOCAL_MODEL = 'willqiu/Llama-Breeze2-8B-Instruct';
 module.exports = { LOCAL_MODEL };
+
 
